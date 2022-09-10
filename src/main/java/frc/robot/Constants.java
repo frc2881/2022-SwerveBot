5fa4b1653410f08d18eee582ff6b7ac659d3f30c
ladycans <31555432+ladycans@users.noreply.github.com>
2022-09-09 19:52:09 -0500

    Base Code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,95 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = Units.inchesToMeters(20.25); // FIXME Measure and set trackwidth
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(21.25); // FIXME Measure and set wheelbase
+
+    //public static final int DRIVETRAIN_PIGEON_ID = 0; // FIXME Set Pigeon ID
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; // FIXME Set front left module drive motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; // FIXME Set front left module steer motor ID
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 2; // FIXME Set front left steer encoder ID
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(72.1); // FIXME Measure and set front left steer offset
+
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 5; // FIXME Set front right drive motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6; // FIXME Set front right steer motor ID
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 6; // FIXME Set front right steer encoder ID
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(209.7); // FIXME Measure and set front right steer offset
+
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3; // FIXME Set back left drive motor ID
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 4; // FIXME Set back left steer motor ID
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 4; // FIXME Set back left steer encoder ID
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(281.3); // FIXME Measure and set back left steer offset
+
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7; // FIXME Set back right drive motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; // FIXME Set back right steer motor ID
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 8; // FIXME Set back right steer encoder ID
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(159.6); // FIXME Measure and set back right steer offset
+
+    public static final class Feeder {
+    public static final int frontCargoFeeder = 10;
+    public static final int rearCargoFeeder = 9;
+    }
+
+    public static final int adjustableHood = 12;
+
+    public static final int topRollers = 13;
+
+    public static final int mainFlywheel = 14;
+
+    public static final int turret = 15;
+
+      /**
+   * Configuration of the intake subsystem.
+   */
+  public static final class Intake {
+    /**
+     * The CAN ID of the intake motor.
+     */
+    public static final int intakeRollers = 11;
+
+    /**
+     * The pneumatic hub channel ID of the intake solenoid.
+     */
+    public static final int kSolenoid = 0;
+
+    /**
+     * The maximum current to send to the intake motor.
+     */
+    public static final int kCurrentLimit = 30;
+
+    /**
+     * The maximum speed that the intake motor runs.
+     */
+    public static final double kMaxSpeed = 0.10;
+  }
+
+    /**
+   * Controls the logging of detailed information about the robot.
+   */
+  public static final boolean kEnableDetailedLogging = true;
+}
---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,131 @@
+// Copyright (c) 2022 FRC Team 2881 - The Lady Cans
+//
+// Open Source Software; you can modify and/or share it under the terms of BSD
+// license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.kEnableDetailedLogging;
+import static frc.robot.Constants.Intake.kCurrentLimit;
+import static frc.robot.Constants.Intake.kMaxSpeed;
+import static frc.robot.Constants.Intake.intakeRollers;
+import static frc.robot.Constants.Intake.kSolenoid;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.util.datalog.BooleanLogEntry;
+import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  private final CANSparkMax m_intake;
+  private final Solenoid m_solenoid;
+  private final DoubleLogEntry m_logOutput;
+  private final DoubleLogEntry m_logBusVoltage;
+  private final DoubleLogEntry m_logCurrent;
+  private final BooleanLogEntry m_logSolenoid;
+  private boolean shootExtend = false;
+  private boolean buttonExtend = false;
+  //private int counter = 0;
+
+  public Intake() {
+    m_intake = new CANSparkMax(intakeRollers, MotorType.kBrushless);
+    m_intake.restoreFactoryDefaults();
+    m_intake.setInverted(false);
+    m_intake.setIdleMode(IdleMode.kBrake);
+    m_intake.setSmartCurrentLimit(kCurrentLimit);
+
+    m_solenoid = new Solenoid(PneumaticsModuleType.REVPH, kSolenoid);
+
+    if(kEnableDetailedLogging) {
+      DataLog log = DataLogManager.getLog();
+      m_logOutput = new DoubleLogEntry(log, "/intake/output");
+      m_logBusVoltage = new DoubleLogEntry(log, "/intake/busVoltage");
+      m_logCurrent = new DoubleLogEntry(log, "/intake/current");
+      m_logSolenoid = new BooleanLogEntry(log, "/intake/solenoid");
+    } else {
+      m_logOutput = null;
+      m_logBusVoltage = null;
+      m_logCurrent = null;
+      m_logSolenoid = null;
+    }
+  }
+
+  @Override
+  public void periodic() {
+    if(kEnableDetailedLogging) {
+      m_logOutput.append(m_intake.getAppliedOutput());
+      m_logBusVoltage.append(m_intake.getBusVoltage());
+      m_logCurrent.append(m_intake.getOutputCurrent());
+    }
+  }
+
+  public void reset() {
+    run(0.0);
+    shootExtend = false;
+    buttonExtend = false;
+    retract();
+  }
+
+  public void run(double speed) {
+    if(speed > kMaxSpeed) {
+      speed = kMaxSpeed;
+    }
+    m_intake.set(-speed);
+  }
+
+  public void runReverse(double speed) {
+    m_intake.set(speed);
+  }
+
+  public void extend() {
+    buttonExtend = true;
+      m_solenoid.set(true);
+      if(kEnableDetailedLogging) {
+        m_logSolenoid.append(m_solenoid.get());
+      }
+  }
+
+  public void retract() {
+    buttonExtend = false;
+      if(shootExtend == false){
+              m_solenoid.set(false);
+      if(kEnableDetailedLogging) {
+        m_logSolenoid.append(m_solenoid.get());
+      }
+    }
+  }
+
+  public void shootExtend() {
+    shootExtend = true;
+      m_solenoid.set(true);
+      if(kEnableDetailedLogging) {
+        m_logSolenoid.append(m_solenoid.get());
+      }
+  }
+
+  public void shootRetract() {
+    shootExtend = false;
+      if(buttonExtend == false){
+              m_solenoid.set(false);
+      if(kEnableDetailedLogging) {
+        m_logSolenoid.append(m_solenoid.get());
+      }
+    }
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+
+    builder.addDoubleProperty("Current", () -> m_intake.getOutputCurrent(),
+                              null);
+  }
+}
