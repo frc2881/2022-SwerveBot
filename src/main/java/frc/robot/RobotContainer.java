f15c0c672c432191ad5f910386e2a2547d214130
Yuqing Liu <frc2881@gmail.com>
2022-09-16 20:40:46 -0500

    Add ring lights

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -24,6 +26,7 @@ import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Launcher;
 import frc.robot.subsystems.Pneumatics;
+import frc.robot.subsystems.PrettyLights;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -33,11 +36,14 @@ import frc.robot.subsystems.Pneumatics;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private final PowerDistribution powerHub = new PowerDistribution(2, ModuleType.kRev);
+  
   private final Launcher m_launcher = new Launcher();
   private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
   private final Intake m_intake = new Intake();
   private final Pneumatics m_pneumatics = new Pneumatics();
   private final Feeder m_feeder = new Feeder();
+  private final PrettyLights m_lights = new PrettyLights(powerHub);
 
   private final XboxController driverController = new XboxController(0);
   private final XboxController manipulatorController = new XboxController(1);
